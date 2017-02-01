f8041975f8fb2e976844b30a104698e21f59d529
Jonathan Sprinkle <sprinkjm@email.arizona.edu>
2017-01-31 20:55:52 -0800

    removing generated files

--- simulink/obstacleStopper_ert_rtw/html/traceInfo_flag.js
+++
@@ -1,6 +0,0 @@
-function TraceInfoFlag() {
-    this.traceFlag = new Array();
-    this.traceFlag["obstacleStopper.cpp:138c37"]=1;
-    this.traceFlag["obstacleStopper.cpp:166c49"]=1;
-}
-top.TraceInfoFlag.instance = new TraceInfoFlag();
